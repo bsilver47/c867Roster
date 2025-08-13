c228a39da8ae8955883e7df2e427fad54de0f2d4
Benjamin Silver <bsilve10@wgu.edu>
2025-08-13 15:31:42 -0600

    This is to include the requirements for step B in the requirements for the assignment. Note: the actual changes are simply that my information (meta data) was added to the top in comments: Step B was implemented in the initial creation of the file/repository.

--- c867Roster/c867Roster.cpp
+++ c867Roster/c867Roster.cpp
@@ -1,5 +1,7 @@
+// Written by Benjamin Sterling Silver
+// This program is part of the C867 Scripting and Programming - Applications course at Western Governors University
+
 // c867Roster.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
 
