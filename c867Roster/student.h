a66514f81f3eced30ce736ad23a0d7711c376e2e
Benjamin Silver <bsilve10@wgu.edu>
2025-08-13 17:21:50 -0600

    Step E

--- c867Roster/student.h
+++ c867Roster/student.h
@@ -1,5 +1,18 @@
+#include <iostream>
+
 enum class DegreeProgram {
 	degree1 = "SECURITY",
 	degree2 = "NETWORK",
 	degree3 = "SOFTWARE"
+};
+
+class Student {
+public:
+	string studentID;
+	string firstName;
+	string lastName;
+	string email;
+	int age;
+	int daysInCourse[3];
+	DegreeProgram degreeProgram;
 };
