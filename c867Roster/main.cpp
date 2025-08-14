ef0487429608014b03534c89feadd2dcb9bcef7c
Benjamin Silver <bsilve10@wgu.edu>
2025-08-13 22:52:50 -0600

    Step G 1&2

---
+++ c867Roster/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <"degree.h">
+#include <"student.h">
+#include <"student.cpp">
+#include <"roster.h">
+#include <"roster.cpp">
+
+
+void main() {
+	// Step 1: Print the header
+	std::cout << "Course: C867 - Scripting and Programming - Applications \n";
+	std::cout << "Language: C++ \n";
+	std::cout << "Student ID: 012359331 \n";
+	std::cout << "Name: Benjamin Sterling Silver \n";
+
+	// Step 2: Create an instance of Roster
+	Roster classRoster;
+	// Step 3: Add students to the roster
+		
+}
