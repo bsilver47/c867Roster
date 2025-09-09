19343012305a3ee4a84ef46f4b698346778f95b8
Benjamin Silver <bsilve10@wgu.edu>
2025-09-09 16:45:27 -0600

    After fixing what may be all of the bugs, this commit is before deleting unnecessary comments and code

--- c867Roster/student.cpp
+++ c867Roster/student.cpp
@@ -1,1 +1,10 @@
+#include "student.h"
 
+string degreeProgramToString(DegreeProgram degree) {
+	switch (degree) {
+	case DegreeProgram::SECURITY: return "SECURITY";
+	case DegreeProgram::NETWORK: return "NETWORK";
+	case DegreeProgram::SOFTWARE: return "SOFTWARE";
+	default: return "UNKNOWN";
+	}
+}
--- c867Roster/student.h
+++ c867Roster/student.h
@@ -19,14 +19,7 @@ enum class DegreeProgram {
 	SOFTWARE
 };
 
-string degreeProgramToString(DegreeProgram degree) {
-	switch (degree) {
-		case DegreeProgram::SECURITY: return "SECURITY";
-		case DegreeProgram::NETWORK: return "NETWORK";
-		case DegreeProgram::SOFTWARE: return "SOFTWARE";
-		default: return "UNKNOWN";
-	}
-}
+string degreeProgramToString(DegreeProgram degree);
 
 class Student {
 public:
